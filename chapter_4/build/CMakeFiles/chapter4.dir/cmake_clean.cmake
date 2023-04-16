6463d3fe130f9d8ad2e04833fe20fe9709c64294
TheWangYang <wyysuccess2020@163.com>
2023-04-16 14:44:51 +0800

    Update the chapter4.cpp.

---
+++ chapter_4/build/CMakeFiles/chapter4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter4.dir/chapter4.cpp.obj"
+  "CMakeFiles/chapter4.dir/chapter4.cpp.obj.d"
+  "chapter4.exe"
+  "chapter4.exe.manifest"
+  "chapter4.pdb"
+  "libchapter4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
